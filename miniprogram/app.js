ec695dad13552020c2c007dc0d3c2beb0bd8c231
AaronW <zmtzwb@gmail.com>
2019-11-18 14:09:51 +0800

    f

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,15 @@
 //app.js
 App({
+  data(){
+    systemInfo: {}
+  },
   onLaunch: function () {
     wx.cloud.init({
       traceUser: true
     })
 
+    let info = wx.getSystemInfoSync();
+  
+    this.data.systemInfo = info;
   }
 })
--- miniprogram/pages/query/query.js
+++ miniprogram/pages/query/query.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name: ''
+    name: '',
+    isWxWork: false
+  },
+
+  onShow(){
+    let info = getApp().data.systemInfo;
+    
+    if(info.environment === "wxwork"){
+      this.setData({
+        isWxWork: true
+      })
+    }
   },
 
   /**
@@ -19,6 +30,11 @@ Page({
       name: e.detail.value
     })
   },
+  handleManageBtn(){
+    wx.navigateTo({
+      url: "/pages/admin/admin"
+    });
+  },
   handleQuery(e){
     if(!this.data.name){
       return;
@@ -26,5 +42,28 @@ Page({
     wx.navigateTo({
       url: "/pages/queryDetail/queryDetail?name="+this.data.name
     });
+  },
+  handleQyLogin(){
+    wx.qy.login({
+      success: function(res){
+        // success
+        console.log(res)
+
+        wx.qy.getEnterpriseUserInfo ({
+          success: function(res) {
+            var userInfo = res.userInfo
+            var name = userInfo.name
+            var gender = userInfo.gender //性别 0：未知、1：男、2：女
+          }
+        })
+      },
+      fail: function() {
+        // fail
+        console.log('failed')
+      },
+      complete: function() {
+        // complete
+      }
+    })
   }
 })
